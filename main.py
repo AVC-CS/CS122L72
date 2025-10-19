123f162c92aca713131f42bb4a64055449924c50
Kyu Lee <kwooler@gmail.com>
2025-10-18 23:19:15 -0700

    L72 init

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-
-
+import random
 def getRandom(N):
     """
     ########################################
     Code Your Program here
     ########################################
     """
+    numbers = [random.randint(1, 100) for _ in range(N)]
+    return numbers
 
 
 def findMin(numbers):
@@ -14,6 +15,8 @@ def findMin(numbers):
     Code Your Program here
     ########################################
     """
+    minidx = numbers.index(min(numbers))
+    numbers[0], numbers[minidx] = numbers[minidx], numbers[0]
 
 
 def main():
@@ -21,7 +24,9 @@ def main():
     N = 5
     numbers = getRandom(N)
     print(f'The original list values : { numbers }')
+    print('Finding the minimum value and swapping it with the first element...')
     findMin(numbers)
+    print(f'The min value is now at the first index: { numbers[0] }')
     print(f'The list values after findMin() : { numbers }')
 
 
--- main_test.py
+++ main_test.py
@@ -1,6 +1,7 @@
 import main
+import pytest
 
-
+@pytest.mark.basic
 def test_main_1():
     N = 5
     numbers = main.getRandom(N)
@@ -17,9 +18,27 @@ def test_main_1():
     assert numbers[0] == minval
     assert numbers[minidx] == firstelm
 
-
+@pytest.mark.edge
 def test_main_2():
     N = 10
+    numbers = main.getRandom(N)
+
+    minval = min(numbers)
+    minidx = numbers.index(minval)
+    firstelm = numbers[0]
+
+    print(f'The original list values {numbers}')
+    main.findMin(numbers)
+    print(f'Min val: {numbers[0]}, Swapped with {numbers[minidx]}')
+    print(f'The list values  after findMin() {numbers}')
+
+    assert numbers[0] == minval
+    assert numbers[minidx] == firstelm
+
+@pytest.mark.bonus
+def test_main_3():
+    
+    N = 1
     numbers = main.getRandom(N)
 
     minval = min(numbers)
